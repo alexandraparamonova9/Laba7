17e35b2915e9b9f5ca2985f0de24db6b7a163a3f
Александра <alexandra918918@gmail.com>
2025-12-14 19:27:54 +0300

    test_currencies.py

--- test_currencies.py
+++ test_currencies.py
@@ -1,8 +1,19 @@
 import unittest
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+from currencies import get_currencies
 
-if __name__ == '__main__':
-    unittest.main()
+
+class TestCurrencies(unittest.TestCase):
+    def test_real_currency(self):
+        result = get_currencies(["USD"])
+        self.assertIn("USD", result)
+
+    def test_invalid_url(self):
+        with self.assertRaises(ConnectionError):
+            get_currencies(["USD"], url="https://invalid")
+
+    def test_missing_currency(self):
+        with self.assertRaises(KeyError):
+            get_currencies(["XXX"])
+
+
